c237a09c471aa62f80d3768da4f985d288c95f0f
ROllerozxa <rollerozxa@voxelmanip.se>
2022-08-20 16:16:37 +0200

    Remove unused libraries and other stuff

--- src/android-lvl/sample/gen/com/example/android/market/licensing/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.android.market.licensing;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
