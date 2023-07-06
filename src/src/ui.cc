a010794b58091ce41aa2b27e7e18ed93ba632d61
griffi-gh <prasol258@gmail.com>
2023-07-07 01:03:53 +0200

    fix

--- src/src/ui.cc
+++ src/src/ui.cc
@@ -389,14 +389,12 @@ bool ui::_imgui_event(tms_event* event) {
         }
         case TMS_EV_KEY_PRESS:
         case TMS_EV_KEY_UP: {
-            //TODO fix this:
             io.AddKeyEvent(ImGuiMod_Shift, (event->data.key.mod & TMS_MOD_SHIFT) != 0);
             io.AddKeyEvent(ImGuiMod_Ctrl, (event->data.key.mod & TMS_MOD_CTRL) != 0);
             io.AddKeyEvent(ImGuiMod_Alt, (event->data.key.mod & TMS_MOD_ALT) != 0);
             io.AddKeyEvent(ImGuiMod_Super, (event->data.key.mod & TMS_MOD_GUI) != 0);
-
             ImGuiKey keycode = tms_key_to_imgui(event->data.key.keycode);
-            io.AddKeyEvent(keycode, event->type == TMS_EV_KEY_DOWN);
+            io.AddKeyEvent(keycode, event->type == (TMS_EV_KEY_PRESS | TMS_EV_KEY_DOWN));
             //XXX: we won't bother supporting SetKeyEventNativeData, as it's only used by legacy user code
             return io.WantCaptureKeyboard;
         }
