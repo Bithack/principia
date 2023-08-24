0b5902dc65c2e4d823353b4a2227a47bf815beeb
ROllerozxa <rollerozxa@voxelmanip.se>
2023-08-24 14:27:05 +0200

    Cleanup .gitignores

--- build-linux/create_png.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-gprof2dot --format=callgrind --output=out.dot $1 &&
-dot -Tpng out.dot -o graph.png
--- build-windows/vs/principia/default_defines.h
+++
@@ -1,7 +0,0 @@
-#define TMS_BACKEND_WINDOWS
-#define TMS_BACKEND_PC
-#define _USE_MATH_DEFINES
-#define TMS_FAST_MATH
-
-// stop windows.h from interfering with std::min and std::max
-#define NOMINMAX
