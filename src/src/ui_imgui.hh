e32a11d26d0fb1206f0a1a9282138e4e85d2b071
griffi-gh <prasol258@gmail.com>
2023-09-05 00:09:31 +0200

    move sandbox menu and lvlman

--- src/src/ui_imgui.hh
+++ src/src/ui_imgui.hh
@@ -3,121 +3,401 @@
 #include <SDL_opengl.h>
 #include <SDL_syswm.h>
 #include "imgui.h"
+//#include "imgui_internal.h"
 #include "imgui_stdlib.h"
 #include "imgui_impl_opengl3.h"
 #include "ui_imgui_impl_tms.hh"
 
-// class UiBase {
-//   public:
-//     static inline void init() {}
-//     static inline void open() {}
-//     static inline void layout() {}
-// };
+//STUFF
+static bool __ec9f6917_true = true;
+#define REF_TRUE &(__ec9f6917_true = true)
+#define MODAL_FLAGS (ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse)
 
-// class UiOpenable: public UiBase {
-//   protected:
-//     static bool is_open;
-  
-//   public:
-//     static inline void open() {
-//       is_open = true;
-//     }
-// };
+//HELPER FUNCTIONS
 
-// class UiPopup: public UiBase {
-//   private:
-//     static bool do_open;
-
-//   protected:
-//     static const char* name;
-//     static inline void popup_layout() {};
-
-//   public:
-//     static inline void open() {
-//       do_open = true;
-//     }
-//     static inline void layout() {
-//       if (do_open) {
-//         do_open = false;
-//         ImGui::OpenPopup(name);
-//       }
-//       if (ImGui::BeginPopup(name, ImGuiWindowFlags_NoMove)) {
-//         popup_layout();
-//         ImGui::EndPopup();
-//       }
-//     }
-// };
-
-// // Implementation
-
-// class UiDemoWindow: public UiOpenable {
-//   public:
-//     static inline void layout() {
-//       if (is_open) ImGui::ShowDemoWindow(&is_open);
-//     }
-// };
-
-// class UiSandboxMenu: public UiPopup {
-//   public:
-//     static inline void popup_layout() {
-//       //True if current level can be saved as a copy
-//       //Saves can only be created if current level state is sandbox
-//       bool is_sandbox = G->state.sandbox;
-
-//       //True if already saved and the save can be updated
-//       //Saves can only be updated if:
-//       // - Current level state is sandbox
-//       // - Level is local (and not an auto-save)
-//       // - Level is already saved
-//       bool can_update_save =
-//           G->state.sandbox &&
-//           (W->level_id_type == LEVEL_LOCAL) &&
-//           (W->level.local_id != 0); //&& W->level.name_len;
-
-//       //TODO info panel
-
-//       //"Level properties"
-//       ImGui::MenuItem("Level properties");
-      
-//       //"Publish online"
-//       if (is_sandbox) {
-//         ImGui::BeginDisabled(!P.user_id);
-//         ImGui::MenuItem("Publish online");
-//         ImGui::EndDisabled();
-//         ImGui::SetItemTooltip("Upload your level to %s", P.community_host);
-//       }
-
-//       //"Save": update current save
-//       if (can_update_save && ImGui::MenuItem("Save")) {
-//         //temporarily change text to "Saved" (green)?
-//         P.add_action(ACTION_SAVE, 0);
-//         ImGui::CloseCurrentPopup();
-//       }
-
-//       //"Save as...": create a new save
-//       if (is_sandbox && ImGui::MenuItem("Save as...")) {
-//         //UiSaveAs::open();
-//         ImGui::CloseCurrentPopup();
-//       }
-
-//       //"Open...": open the Level Manager
-//       if (ImGui::MenuItem("Open...")) {
-//         //UiSaveManager::open();
-//         ImGui::CloseCurrentPopup();
-//       }
-
-//       ImGui::Separator();
-//     }
-// };
-
-static void ui_init() {
-  // UiDemoWindow::init();
-  // UiSandboxMenu::init();
+static bool lax_search(const std::string& where, const std::string& what) {
+  return std::search(
+    where.begin(), where.end(),
+    what.begin(), what.end(),
+    [](char lhs, char rhs) { return std::tolower(lhs) == std::tolower(rhs); }
+  ) != where.end();
 }
 
+static void ImGui_CenterNextWindow() {
+  ImGuiIO& io = ImGui::GetIO();
+  ImGui::SetNextWindowPos(
+    ImVec2(io.DisplaySize.x * 0.5f, io.DisplaySize.y * 0.5f),
+    ImGuiCond_Always, //ImGuiCond_Appearing,
+    ImVec2(0.5f, 0.5f)
+  );
+}
+
+namespace UiSandboxMenu  { static void open(); static void layout(); }
+namespace UiLevelManager { static void open(); static void layout(); }
+
+namespace UiSandboxMenu {
+  static bool do_open = false;
+  static b2Vec2 sb_position = b2Vec2_zero;
+
+  static void open() {
+    do_open = true;
+    sb_position = G->get_last_cursor_pos(0);
+  }
+
+  static void layout() {
+    if (do_open) {
+      do_open = false;
+      ImGui::OpenPopup("sandbox_menu");
+    }
+    if (ImGui::BeginPopup("sandbox_menu", ImGuiWindowFlags_NoMove)) {
+      //True if current level can be saved as a copy
+      //Saves can only be created if current level state is sandbox
+      bool is_sandbox = G->state.sandbox;
+
+      //True if already saved and the save can be updated
+      //Saves can only be updated if:
+      // - Current level state is sandbox
+      // - Level is local (and not an auto-save)
+      // - Level is already saved
+      bool can_update_save =
+          G->state.sandbox &&
+          (W->level_id_type == LEVEL_LOCAL) &&
+          (W->level.local_id != 0); //&& W->level.name_len;
+
+      //Info panel
+
+      //Cursor:
+      ImGui::Text("Cursor: (%.2f, %.2f)", sb_position.x, sb_position.y);
+      ImGui::Separator();
+
+      //Selected object info:
+      if (G->selection.e) {
+        //If an object is selected, display it's info...
+        //XXX: some of this stuff does the same things as principia ui items...
+        //---- consider removal?
+        entity* sent = G->selection.e;
+        b2Vec2 sent_pos = sent->get_position();
+        ImGui::Text("%s (g_id: %d)", sent->get_name(), sent->g_id);
+        ImGui::Text("ID: %d", sent->id);
+        ImGui::Text("Position: (%.2f, %.2f)", sent_pos.x, sent_pos.y);
+        if ((sent->dialog_id > 0) && ImGui::MenuItem("Configure...")) {
+          ui::open_dialog(sent->dialog_id);
+        }
+        if (ImGui::MenuItem("> Move to cursor")) {
+          G->selection.e->set_position(sb_position);
+        };
+        ImGui::Separator();
+      }
+
+
+      //"Level properties"
+      if (ImGui::MenuItem("Level properties")) {
+        //TODO
+      }
+      
+      //"Publish online"
+      if (is_sandbox) {
+        ImGui::BeginDisabled(!P.user_id);
+        ImGui::MenuItem("Publish online");
+        ImGui::EndDisabled();
+        ImGui::SetItemTooltip("Upload your level to %s", P.community_host);
+      }
+
+      //"Save": update current save
+      if (can_update_save && ImGui::MenuItem("Save")) {
+        //XXX: temporarily change text to "Saved" (green)?
+        P.add_action(ACTION_SAVE, 0);
+        ImGui::CloseCurrentPopup();
+      }
+
+      //"Save as...": create a new save
+      if (is_sandbox && ImGui::MenuItem("Save as...")) {
+        //UiSaveAs::open();
+        ImGui::CloseCurrentPopup();
+      }
+
+      //"Open...": open the Level Manager
+      if (ImGui::MenuItem("Open...")) {
+        UiLevelManager::open();
+        ImGui::CloseCurrentPopup();
+      }
+
+      ImGui::Separator();
+      
+      //"User menu": This menu is basically useless/just a placeholder
+      if (P.user_id && P.username) {
+        ImGui::PushID("##UserMenu");
+        if (ImGui::BeginMenu(P.username)) {
+          if (ImGui::MenuItem("Manage account")) {
+            char tmp[1024];
+            snprintf(tmp, 1023, "https://%s/user/%s", P.community_host, P.username);
+            ui::open_url(tmp);
+          }
+          if (ImGui::MenuItem("Log out")) {
+            //TODO actually log out
+            P.user_id = 0;
+            P.username = nullptr;
+            P.add_action(ACTION_REFRESH_HEADER_DATA, 0);
+          }
+          ImGui::EndMenu();
+        };
+        ImGui::PopID();
+      } else {
+        if (ImGui::MenuItem("Log in")) {
+          //TODO
+        };
+      }
+
+      if (ImGui::MenuItem("Settings")) {
+        //TODO
+      }
+
+      ImGui::Separator();
+      
+      if (ImGui::MenuItem("Back to menu")) {
+        P.add_action(ACTION_GOTO_MAINMENU, 0);
+      };
+
+      ImGui::EndMenu();
+    }
+  }
+};
+
+
+namespace UiLevelManager {
+  struct lvlinfo_ext {
+    lvlinfo info;
+    uint32_t id;
+    int type;
+  };
+
+  static bool do_open = false;
+  static std::string search_query{""};
+
+  static lvlfile *level_list = nullptr;
+  static int level_list_type = LEVEL_LOCAL;
+
+  static lvlinfo_ext *level_metadata = nullptr;
+
+  static void update_level_info(int id_type, uint32_t id) {
+    if (level_metadata) {
+      //Check if data needs to be reloaded
+      if ((level_metadata->id == id) && (level_metadata->type == id_type)) return;
+
+      //Dealloc current data
+      level_metadata->info.~lvlinfo();
+      free(level_metadata);
+    }
+    
+    level_metadata = new lvlinfo_ext;
+
+    //Update meta
+    level_metadata->id = id;
+    level_metadata->type = id_type;
+    
+    //Read level info
+    lvledit lvl;
+    if (lvl.open(id_type, id)) {
+      level_metadata->info = lvl.lvl;
+      if (level_metadata->info.descr_len && level_metadata->info.descr) {
+        level_metadata->info.descr = strdup(level_metadata->info.descr);
+      }
+    } else {
+      delete level_metadata;
+      level_metadata = nullptr;
+    }
+  }
+
+  static void reload_level_list() {
+    //Recursively deallocate the linked list
+    while (level_list) {
+      lvlfile* next = level_list->next;
+      delete level_list;
+      level_list = next;
+    }
+    //Get a new list of levels
+    level_list = pkgman::get_levels(level_list_type);
+  }
+
+  static void open() {
+    do_open = true;
+    search_query = "";
+    level_list_type = LEVEL_LOCAL;
+    reload_level_list();
+  }
+
+  static void layout() {
+    ImGuiIO& io = ImGui::GetIO();
+    if (do_open) {
+      do_open = false;
+      ImGui::OpenPopup("Level Manager");
+    }
+    ImGui_CenterNextWindow();
+    ImGui::SetNextWindowSize(ImVec2(800., 0.));
+    if (ImGui::BeginPopupModal("Level Manager", REF_TRUE, MODAL_FLAGS)) {
+      bool any_level_found = false;
+      
+      //Top action bar
+      {
+        //Level type selector
+        //Allows switching between local and DB levels
+        static const char* items[] = { "Local", "Downloaded" };
+        if (ImGui::Combo("##id-lvltype", &level_list_type, items, IM_ARRAYSIZE(items))) {
+          reload_level_list();
+        }
+
+        //"Get more levels" button
+        ImGui::SameLine();
+        if ((level_list_type == LEVEL_DB) && ImGui::Button("Get more levels"))
+          ui::open_url((std::string("https://") + P.community_host).c_str());
+
+        //Align stuff to the right
+        //lvlname width + padding
+        ImGui::SameLine();
+        ImGui::SetCursorPosX(ImGui::GetWindowWidth() - (200. + 10.));
+        
+        //Actual level name field
+        ImGui::PushItemWidth(200.);
+        ImGui::InputTextWithHint("##LvlmanLevelName", "Search levels", &search_query);
+        ImGui::PopItemWidth();
+      }
+
+      ImGui::Separator();
+
+      //Actual level list
+      ImGui::BeginChild("save_list_child", ImVec2(0., 500.), false);
+      if (ImGui::BeginTable("save_list", 5, ImGuiTableFlags_Borders)) {
+        //Setup table columns
+        ImGui::TableSetupColumn("ID", ImGuiTableColumnFlags_WidthFixed);
+        ImGui::TableSetupColumn("Name", ImGuiTableColumnFlags_WidthStretch);
+        ImGui::TableSetupColumn("Last modified", ImGuiTableColumnFlags_WidthFixed);
+        ImGui::TableSetupColumn("Version", ImGuiTableColumnFlags_WidthFixed);
+        ImGui::TableSetupColumn("Actions", ImGuiTableColumnFlags_WidthFixed);
+        ImGui::TableHeadersRow();
+
+        lvlfile *level = level_list;
+        while (level) {
+          //Search (lax_search is used to ignore case)
+          if ((search_query.length() > 0) && !(
+            lax_search(level->name, search_query) ||
+            (std::to_string(level->id).find(search_query) != std::string::npos)
+          )) {
+            //Just skip levels we don't like
+            level = level->next;
+            continue;
+          }
+
+          //This is required to prevent ID conflicts
+          ImGui::PushID(level->id);
+
+          //Start laying out the table row...
+          ImGui::TableNextRow();
+
+          //ID
+          if (ImGui::TableNextColumn()) {
+            ImGui::Text("%d", level->id);
+          }
+
+          //Name
+          if (ImGui::TableNextColumn()) {
+            ImGui::SetNextItemWidth(999.);
+            ImGui::LabelText("##levelname", "%s", level->name);
+
+            //Display description if hovered
+            if (ImGui::BeginItemTooltip()) {
+              update_level_info(level->id_type, level->id);
+              if (!level_metadata) {
+                ImGui::TextColored(ImVec4(1.,.3,.3,1.), "Failed to load level metadata");
+              } else if (level_metadata->info.descr_len && level_metadata->info.descr) {
+                ImGui::PushTextWrapPos(400);
+                ImGui::TextWrapped("%s", level_metadata->info.descr);
+                ImGui::PopTextWrapPos();
+              } else {
+                ImGui::TextColored(ImVec4(.6,.6,.6,1.), "<no description>");
+              }
+              ImGui::EndTooltip();
+            }
+          }
+
+          //Modified date
+          if (ImGui::TableNextColumn()) {
+            ImGui::TextUnformatted(level->modified_date);
+          }
+
+          //Version
+          if (ImGui::TableNextColumn()) {
+            const char* version_str = level_version_string(level->version);
+            if (version_str == "unknown_version") version_str = "unknown";
+            if (version_str == "old_level") version_str = "old";
+            ImGui::Text("%s (%d)", version_str, level->version);
+          }
+
+          //Actions
+          if (ImGui::TableNextColumn()) {
+            // Delete level ---
+            // To prevent accidental level deletion,
+            // Shift must be held while clicking the button
+            bool allow_delete = io.KeyShift;
+            ImGui::PushStyleVar(ImGuiStyleVar_Alpha, allow_delete ? 1. : .6);
+            if (ImGui::Button("Delete##delete-sandbox-level")) {
+              G->lock();
+              if (allow_delete && G->delete_level(level->id_type, level->id, level->save_id)) {
+                //If deleting current local level, remove it's local_id
+                //This disables the "save" option
+                if ((level->id_type == LEVEL_LOCAL) && (level->id == W->level.local_id)) {
+                  W->level.local_id = 0;
+                }
+                //Reload the list of levels
+                reload_level_list();
+              }
+              G->unlock();
+            }
+            ImGui::PopStyleVar();
+            if (!allow_delete) ImGui::SetItemTooltip("Hold Shift to unlock");
+
+            //TODO "Play" button
+
+            // Open level ---
+            // Principia's ACTION_OPEN signal only supports loading local levels,
+            // so we have to lock the game and load the level manually...
+            // this is completely fine unless the gui is multithreaded
+            ImGui::SameLine();
+            if (ImGui::Button("Open level")) {
+              if (level->id_type == LEVEL_LOCAL) {
+                //Use ACTION_OPEN if possible
+                P.add_action(ACTION_OPEN, level->id);
+              } else {
+                //Otherwise, load the level and switch the screen manually
+                G->lock();
+                G->open_sandbox(level->id_type, level->id);
+                G->resume_action = GAME_RESUME_OPEN;
+                tms::set_screen(G);
+                P.s_loading_screen->set_next_screen(G);
+                G->unlock();
+              }
+              ImGui::CloseCurrentPopup();
+            }
+          }
+
+          level = level->next;
+          any_level_found = true;
+
+          ImGui::PopID();
+        }
+        ImGui::EndTable();
+        ImGui::EndChild();
+      }
+      if (!any_level_found) {
+        ImGui::TextUnformatted("No levels found");
+      }
+      ImGui::EndPopup();
+    }
+  }
+};
+
 static void ui_layout() {
-  // UiDemoWindow::layout();
-  // UiSandboxMenu::layout();
+  UiSandboxMenu::layout();
+  UiLevelManager::layout();
 }
 
 //*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
