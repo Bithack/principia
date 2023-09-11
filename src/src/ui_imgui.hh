f7bb1b4f065135643a4bbe551619936c28be2501
griffi-gh <prasol258@gmail.com>
2023-09-12 00:25:43 +0200

    fix tips dialog on uiscale != 1

--- src/src/ui_imgui.hh
+++ src/src/ui_imgui.hh
@@ -1156,10 +1156,15 @@ namespace UiTips {
     ImGui_CenterNextWindow();
     ImGui::SetNextWindowSize(ImVec2(400, 200));
     if (ImGui::BeginPopupModal("Tips and tricks", REF_TRUE, MODAL_FLAGS)) {
+      ImGuiStyle &style = ImGui::GetStyle();
+      float font_size = ImGui::GetFontSize();
+      ImVec2 frame_padding = style.FramePadding;
+      ImVec2 content_region = ImGui::GetContentRegionMax();
+
       ImGui::TextWrapped("%s", tips[ctip]);
+
       //Align at the bottom of the window
-      //TODO do not hardcode y
-      ImGui::SetCursorPosY(ImGui::GetWindowSize().y - 27);
+      ImGui::SetCursorPosY(content_region.y - (font_size + (2. * frame_padding.y)));
       if (ImGui::Button("<###tips-prev")) {
         if (--ctip < 0) ctip = num_tips - 1;
       }
@@ -1175,7 +1180,8 @@ namespace UiTips {
       }
       ImGui::SameLine();
       const char* close_text = "Close";
-      ImGui::SetCursorPosX(ImGui::GetWindowSize().x - ImGui::CalcTextSize(close_text).x - 15);
+      const ImVec2 close_text_size = ImGui::CalcTextSize(close_text);
+      ImGui::SetCursorPosX(content_region.x - (close_text_size.x + frame_padding.x * 2.));
       if (ImGui::Button(close_text)) {
         ImGui::CloseCurrentPopup();
       }
