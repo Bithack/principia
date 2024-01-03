796354fdc5a18e7f3e7d379a0ce2d0ea62b99a73
ROllerozxa <rollerozxa@voxelmanip.se>
2024-01-03 21:13:07 +0100

    Move LuaScript header code into separate file

---
+++ src/src/luascript_header.lua
@@ -0,0 +1,62 @@
+R"luastring"--(
+
+local EVENT_PLAYER_DIE = 0
+local EVENT_ENEMY_DIE = 1
+local EVENT_INTERACTIVE_DESTROY = 2
+local EVENT_PLAYER_RESPAWN = 3
+local EVENT_CLICK_DOWN = 4
+local EVENT_CLICK_UP = 5
+local EVENT_ABSORB = 6
+
+local INPUT_KEY_DOWN = 4
+local INPUT_KEY_UP = 8
+local INPUT_POINTER_DOWN = 16
+local INPUT_POINTER_UP = 32
+
+local KEY_A = 4
+local KEY_B = 5
+local KEY_C = 6
+local KEY_D = 7
+local KEY_E = 8
+local KEY_F = 9
+local KEY_G = 10
+local KEY_H = 11
+local KEY_I = 12
+local KEY_J = 13
+local KEY_K = 14
+local KEY_L = 15
+local KEY_M = 16
+local KEY_N = 17
+local KEY_O = 18
+local KEY_P = 19
+local KEY_Q = 20
+local KEY_R = 21
+local KEY_S = 22
+local KEY_T = 23
+local KEY_U = 24
+local KEY_V = 25
+local KEY_W = 26
+local KEY_X = 27
+local KEY_Y = 28
+local KEY_Z = 29
+local KEY_1 = 30
+local KEY_2 = 31
+local KEY_3 = 32
+local KEY_4 = 33
+local KEY_5 = 34
+local KEY_6 = 35
+local KEY_7 = 36
+local KEY_8 = 37
+local KEY_9 = 38
+local KEY_0 = 39
+local KEY_ENTER = 40
+local KEY_ESC = 41
+local KEY_BACKSPACE = 42
+local KEY_TAB = 43
+local KEY_SPACE = 44
+local KEY_RIGHT = 79
+local KEY_LEFT = 80
+local KEY_DOWN = 81
+local KEY_UP = 82
+
+--)luastring"--"
