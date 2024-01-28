8afd0d135f6b097ec78ac4604264bbe9e4931684
ROllerozxa <rollerozxa@voxelmanip.se>
2024-01-28 21:45:17 +0100

    Windows and Linux backend cleanups, move some platform-specific compiler flags

--- src/tms/backends/linux/main.cc
+++ src/tms/backends/linux/main.cc
@@ -5,9 +5,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <pwd.h>
-#include <cxxabi.h>
 #include <unistd.h>
-#include <libgen.h>
 
 #include <tms/core/project.h>
 #include <tms/core/event.h>
@@ -15,10 +13,6 @@
 
 #include <tms/backend/opengl.h>
 #include <tms/backends/common.h>
-
-#ifdef DEBUG
-#include <fenv.h>
-#endif
 
 #include "settings.hh"
 #include "main.hh"
@@ -111,11 +105,7 @@ main(int argc, char **argv)
         SDL_CreateThread(_pipe_listener, "_pipe_listener", 0);
     }
 
-    char buf[512];
-    readlink("/proc/self/exe", buf, 511);
-    dirname(buf);
-    tms_infof("chdirring to %s", buf);
-    chdir(buf);
+    CHDIR_EXE;
 
     // Check if we're in the right place
     struct stat st{};
@@ -139,43 +129,17 @@ main(int argc, char **argv)
 
     mkdir(tbackend_get_storage_path(), S_IRWXU | S_IRWXG | S_IRWXO);
 
-    tms_progressf("Initializing SDL... ");
-    SDL_Init(SDL_INIT_VIDEO);
-    tms_progressf("OK\n");
-    SDL_DisplayMode mode;
-    SDL_GetCurrentDisplayMode(0, &mode);
+    INIT_SDL;
 
-    _tms.window_width = 1280;
+    RESIZE_WINDOW;
 
-    if (mode.w <= 1280) {
-        _tms.window_width = (int)((double)mode.w * .9);
-    } else if (mode.w >= 2100 && mode.h > 1100) {
-        _tms.window_width = 1920;
-    }
-    _tms.window_height = (int)((double)_tms.window_width * .5625);
-
-    tms_infof("set initial res to %dx%d", _tms.window_width, _tms.window_height);
-
-    settings.init();
-    settings.load();
-    P.loaded_correctly_last_run = settings["loaded_correctly"]->v.b;
-
-    settings["is_very_shitty"]->v.b = (!settings["loaded_correctly"]->v.b || settings["is_very_shitty"]->v.b);
-    settings["loaded_correctly"]->v.b = false;
-    settings.save();
-
-    tms_infof("Texture quality: %d", settings["texture_quality"]->v.i8);
-    tms_infof("Shadow quality: %d (%dx%d)",
-            settings["shadow_quality"]->v.i8,
-            settings["shadow_map_resx"]->v.i,
-            settings["shadow_map_resy"]->v.i);
+    LOAD_SETTINGS;
 
     tproject_set_args(argc, argv);
     tms_init();
 
-    if (_tms.screen == 0) {
+    if (_tms.screen == 0)
         tms_fatalf("context has no initial screen, bailing out");
-    }
 
     do {
         int i;
@@ -209,7 +173,7 @@ main(int argc, char **argv)
                 case SDL_WINDOWEVENT:
                     switch (ev.window.event) {
                         case SDL_WINDOWEVENT_RESIZED: {
-                            RESIZE_WINDOW;
+                            WINDOW_RESIZED;
                         } break;
                         case SDL_WINDOWEVENT_MAXIMIZED:
                             settings["window_maximized"]->v.b = true;
@@ -278,91 +242,16 @@ tbackend_init_surface()
     _tms.window_width = settings["window_width"]->v.i;
     _tms.window_height = settings["window_height"]->v.i;
 
-    /* default */
     _tms.xppcm = 108.f/2.54f * 1.5f;
     _tms.yppcm = 107.f/2.54f * 1.5f;
 
-    uint32_t flags = 0;
+    CREATE_SDL_WINDOW;
 
-    flags |= SDL_WINDOW_OPENGL;
-    flags |= SDL_WINDOW_SHOWN;
-    flags |= SDL_WINDOW_RESIZABLE;
+    CREATE_GL_CONTEXT;
 
-    if (settings["window_maximized"]->v.b)
-        flags |= SDL_WINDOW_MAXIMIZED;
+    INIT_GLEW;
 
-    tms_progressf("Creating window... ");
-    _window = SDL_CreateWindow("Principia", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _tms.window_width, _tms.window_height, flags);
-
-    if (_window == NULL) {
-        tms_progressf("ERROR: %s\n", SDL_GetError());
-        exit(1);
-    } else
-        tms_progressf("OK\n");
-
-    _tms._window = _window;
-
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-
-    SDL_GLContext gl_context = SDL_GL_CreateContext(_window);
-
-    if (gl_context == NULL) {
-        tms_fatalf("Error creating GL Context: %s", SDL_GetError());
-    }
-
-    if (settings["vsync"]->v.b) {
-        if (SDL_GL_SetSwapInterval(-1) == -1)
-            SDL_GL_SetSwapInterval(1);
-    } else
-        SDL_GL_SetSwapInterval(0);
-
-    tms_progressf("Initializing GLEW... ");
-    glewExperimental = GL_TRUE;
-    GLenum err = glewInit();
-    if (err != GLEW_OK) {
-        tms_progressf("ERROR: %s\n", glewGetErrorString(err));
-        exit(1);
-    }
-    tms_progressf("OK (v%s)\n", glewGetString(GLEW_VERSION));
-
-    tms_infof("GL Info: %s/%s/%s", glGetString(GL_VENDOR), glGetString(GL_RENDERER), glGetString(GL_VERSION));
-    tms_infof("GLSL Version: %s", glGetString(GL_SHADING_LANGUAGE_VERSION));
-    tms_infof("Extensions: %s", glGetString(GL_EXTENSIONS));
-
-    tms_progressf("GL versions supported: ");
-
-    if (GLEW_VERSION_4_4)
-        tms_progressf("4.4, ");
-    if (GLEW_VERSION_4_3)
-        tms_progressf("4.3, ");
-    if (GLEW_VERSION_4_2)
-        tms_progressf("4.2, ");
-    if (GLEW_VERSION_4_1)
-        tms_progressf("4.1, ");
-    if (GLEW_VERSION_3_3)
-        tms_progressf("3.3, ");
-    if (GLEW_VERSION_3_1)
-        tms_progressf("3.1, ");
-    if (GLEW_VERSION_3_0)
-        tms_progressf("3.0, ");
-    if (GLEW_VERSION_2_1)
-        tms_progressf("2.1, ");
-    if (GLEW_VERSION_2_0)
-        tms_progressf("2.0, ");
-    if (GLEW_VERSION_1_5)
-        tms_progressf("1.5, ");
-    if (GLEW_VERSION_1_4)
-        tms_progressf("1.4, ");
-    if (GLEW_VERSION_1_3)
-        tms_progressf("1.3, ");
-    if (GLEW_VERSION_1_2)
-        tms_progressf("1.2, ");
-    if (GLEW_VERSION_1_1)
-        tms_progressf("1.1");
-
-    tms_progressf("\n");
+    PRINT_GL_INFO;
 
     return T_OK;
 }
@@ -515,8 +404,8 @@ T_intercept_input(SDL_Event ev)
                 spec.data.button.button = mouse_down;
             } else {
                 spec.type = TMS_EV_POINTER_MOVE;
-                spec.data.motion.x = ev.motion.x;
-                spec.data.motion.y = motion_y;
+                spec.data.button.x = ev.motion.x;
+                spec.data.button.y = motion_y;
             }
 
             break;
