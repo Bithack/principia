3f8278fa26790df507d4eb11a9fca2e64399bd44
ROllerozxa <rollerozxa@voxelmanip.se>
2024-06-17 22:26:10 +0200

    Remove explicit GL_ACCELERATED_VISUAL=1 attribute
    Makes it possible to fallback to software rendering when hardware acceleration is not available, e.g. in a macOS VM, instead of crashing completely.

--- src/tms/backend/main.cc
+++ src/tms/backend/main.cc
@@ -581,7 +581,6 @@ tbackend_init_surface()
 
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
 
     SDL_GLContext gl_context = SDL_GL_CreateContext(_window);
 
