81233f5f21bcff39c5110c2cf88e1b965edd71e4
ROllerozxa <rollerozxa@voxelmanip.se>
2024-09-01 21:33:41 +0200

    Fix sqrt gate outputting NaN

--- src/src/i1o1gate.cc
+++ src/src/i1o1gate.cc
@@ -179,7 +179,7 @@ sqrtgate::solve_electronics()
     if (!this->s_in[0].is_ready())
         return this->s_in[0].get_connected_edevice();
 
-    float v = this->s_in[0].get_value();
+    float v = tclampf(this->s_in[0].get_value(), 0.f, 1.f);
     v = sqrtf(v);
 
     this->s_out[0].write(v);
