41e7fca1efb0d02243373edfbd002850fda810b0
ROllerozxa <rollerozxa@voxelmanip.se>
2025-04-02 20:22:42 +0200

    Switch to CLANG64 on Windows

---
+++ packaging/windows-install-pkgs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download and install custom built packages with less dependencies than MSYS2 counterparts
+
+REPO=https://github.com/principia-game/windows-deps
+
+wget ${REPO}/releases/download/latest/mingw-w64-clang-x86_64-curl-winssl-8.12.1-1-any.pkg.tar.zst
+wget ${REPO}/releases/download/latest/mingw-w64-clang-x86_64-freetype-2.13.2-1-any.pkg.tar.zst
+
+pacman -U --noconfirm *.pkg.tar.zst
