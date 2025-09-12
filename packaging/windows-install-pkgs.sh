3ca14754dd97edbb7934fa245b16e094a6c363a7
ROllerozxa <rollerozxa@voxelmanip.se>
2025-09-12 17:18:38 +0200

    Bump Windows dependencies

--- packaging/windows-install-pkgs.sh
+++ packaging/windows-install-pkgs.sh
@@ -4,7 +4,7 @@
 
 REPO=https://github.com/principia-game/windows-deps
 
-wget ${REPO}/releases/download/latest/mingw-w64-clang-x86_64-curl-winssl-8.12.1-1-any.pkg.tar.zst
-wget ${REPO}/releases/download/latest/mingw-w64-clang-x86_64-freetype-2.13.2-1-any.pkg.tar.zst
+wget ${REPO}/releases/download/latest/mingw-w64-clang-x86_64-curl-winssl-8.16.0-1-any.pkg.tar.zst
+wget ${REPO}/releases/download/latest/mingw-w64-clang-x86_64-freetype-2.14.1-1-any.pkg.tar.zst
 
 pacman -U --noconfirm *.pkg.tar.zst
