786356968b38e2e8e78edbe1c1f557068f6cf22d
ROllerozxa <rollerozxa@voxelmanip.se>
2026-01-10 19:27:52 +0100

    Switch to imgui_freetype for rendering text in Imgui dialogs

--- lib/imgui/copy.sh
+++ lib/imgui/copy.sh
@@ -9,6 +9,10 @@ cp ${IMGUI}/imconfig.h .
 cp ${IMGUI}/imgui.{cpp,h} .
 cp ${IMGUI}/imgui_{demo,draw,tables,widgets}.cpp .
 cp ${IMGUI}/imgui_internal.h .
-cp ${IMGUI}/imstb_{rectpack,textedit,truetype}.h .
+cp ${IMGUI}/imstb_{rectpack,textedit}.h .
 cp ${IMGUI}/backends/imgui_impl_opengl3{.cpp,.h,_loader.h} .
 cp ${IMGUI}/misc/cpp/imgui_stdlib.{cpp,h} .
+
+mkdir -p misc/freetype
+cp ${IMGUI}/misc/freetype/imgui_freetype.cpp misc/freetype/
+cp ${IMGUI}/misc/freetype/imgui_freetype.h misc/freetype/
