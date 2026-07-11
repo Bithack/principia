c7fd91a64fa46fd8e02e1b5819aaabe26bc9f03f
ROllerozxa <rollerozxa@voxelmanip.se>
2026-07-11 23:58:39 +0200

    Fix heap UaF in Imgui open level dialog, limit list to 5000 levels
    The level list linked list got entirely destroyed when a level is deleted and then recreated, while it tries to iterate over the now destroyed linked list.

--- src/ui/level_manager.cc
+++ src/ui/level_manager.cc
@@ -19,21 +19,21 @@ namespace UiLevelManager {
 
     static int update_level_info(int id_type, uint32_t id) {
         if (level_metadata) {
-            //Check if data needs to be reloaded
+            // Check if data needs to be reloaded
             if (level_metadata->id == id)
                 return 0;
 
-            //Dealloc current data
+            // Dealloc current data
             level_metadata->info.~lvlinfo();
-            free(level_metadata);
+            delete level_metadata;
         }
 
         level_metadata = new lvlinfo_ext;
 
-        //Update meta
+        // Update meta
         level_metadata->id = id;
 
-        //Read level info
+        // Read level info
         lvledit lvl;
         if (lvl.open(id_type, id)) {
             level_metadata->info = lvl.lvl;
@@ -82,9 +82,9 @@ namespace UiLevelManager {
 
                 //Actual level name field
                 ImGui::PushItemWidth(UI(200.));
-                if (ImGui::IsWindowAppearing()) {
+                if (ImGui::IsWindowAppearing())
                     ImGui::SetKeyboardFocusHere();
-                }
+
                 ImGui::InputTextWithHint("##LvlmanLevelName", "Search levels", &search_query);
                 ImGui::PopItemWidth();
             }
@@ -102,14 +102,26 @@ namespace UiLevelManager {
                 ImGui::TableSetupColumn("Actions", ImGuiTableColumnFlags_WidthFixed);
                 ImGui::TableHeadersRow();
 
+                lvlfile *prev_level = nullptr;
                 lvlfile *level = level_list;
+                int num_levels = 0;
+                int skipped_levels = 0;
                 while (level) {
+                    bool pending_reload = false;
+
                     //Search (lax_search is used to ignore case)
                     if ((search_query.length() > 0) && !(
                         lax_search(level->name, search_query) ||
                         (std::to_string(level->id).find(search_query) != std::string::npos)
                     )) {
-                        //Just skip levels we don't like
+                        // Just skip levels we don't like
+                        level = level->next;
+                        continue;
+                    }
+
+                    num_levels++;
+                    if (num_levels > 5000) {
+                        skipped_levels++;
                         level = level->next;
                         continue;
                     }
@@ -151,8 +163,10 @@ namespace UiLevelManager {
                         ImGui::Text("%s", level->modified_date);
 
                     //Version
-                    if (ImGui::TableNextColumn())
-                        ImGui::Text("%s", level_version_string(level->version));
+                    if (ImGui::TableNextColumn()) {
+                        const char *version_str = level_version_string(level->version);
+                        ImGui::Text("%s", version_str);
+                    }
 
                     //Actions
                     if (ImGui::TableNextColumn()) {
@@ -167,8 +181,7 @@ namespace UiLevelManager {
                                     if ((level->id_type == LEVEL_LOCAL) && (level->id == W->level.local_id))
                                         W->level.local_id = 0;
 
-                                    //Reload the list of levels
-                                    reload_level_list();
+                                    pending_reload = true;
                                 }
                             }
                         }
@@ -187,17 +200,37 @@ namespace UiLevelManager {
                         }
                     }
 
-                    level = level->next;
+                    if (pending_reload) {
+                        // delete the current level and reattach the linked list
+                        if (prev_level) {
+                            prev_level->next = level->next;
+                            delete level;
+                            level = prev_level->next;
+                        } else {
+                            // we're deleting the head
+                            lvlfile *next_level = level->next;
+                            delete level;
+                            level_list = next_level;
+                            level = next_level;
+                        }
+                    } else {
+                        prev_level = level;
+                        level = level->next;
+                    }
+
                     any_level_found = true;
 
                     ImGui::PopID();
                 }
                 ImGui::EndTable();
-                if (!any_level_found)
-                    ImGui::TextUnformatted("No levels found");
+                if (skipped_levels > 0)
+                    ImGui::TextDisabled("%s", std::string("(" + std::to_string(skipped_levels) + " levels not shown - search to narrow down results)").c_str());
+                else if (!any_level_found)
+                    ImGui::TextDisabled("No levels found");
 
                 ImGui::EndChild();
             }
+
             ImGui::EndPopup();
         }
     }
