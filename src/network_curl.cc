0212bc7ebdcc27123592b4eb9a8bb7c1de8c5d30
ROllerozxa <rollerozxa@voxelmanip.se>
2026-07-20 21:10:46 +0200

    Disable certificate verification for special non-existant host in debug build

--- src/network_curl.cc
+++ src/network_curl.cc
@@ -234,6 +234,12 @@ static void init_curl_defaults(void *curl) {
 
 #ifdef DEBUG
     curl_easy_setopt(P.curl, CURLOPT_VERBOSE, 1);
+
+    if (strcmp(P.community_host, "principia-web.uwu") == 0) {
+        tms_warnf("[DEBUG] Using local community host 'principia-web.uwu', disabling certificate verification.");
+        curl_easy_setopt(P.curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_easy_setopt(P.curl, CURLOPT_SSL_VERIFYHOST, 0);
+    }
 #endif
 
     // Note: this may put token cookie in the log output
