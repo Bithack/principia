e1b876eb74ae96bbb51fc108f19174005f4b6c2f
ROllerozxa <rollerozxa@voxelmanip.se>
2026-07-23 19:38:39 +0200

    Update Dear Imgui to d94d0f3649c99d9a026518214f796aaacde707fe

--- lib/imgui/README.md
+++ lib/imgui/README.md
@@ -1,6 +1,12 @@
 # Dear Imgui
 These are source files from the Dear Imgui library, for Principia's experimental Dear Imgui dialog backend.
 
-All files are currently taken from commit 93e396ffb75834b4fa79004a4f0fccb600a8e164 and have no modifications done to them compared to upstream.
+All files are currently taken from commit d94d0f3649c99d9a026518214f796aaacde707fe with the following modifications:
+
+- `imconfig.h` is entirely custom and should be kept
+- `imgui_impl_opengl3.cpp`:
+	- Desktop GL / GLES2 is dynamically determined at runtime using `_tms.use_gles`
+
+All downstream modifications should have a `// XXX PRINCIPIA XXX` comment for keeping track of them.
 
 To update from upstream, see the `copy.sh` script in this folder.
--- lib/imgui/imconfig.h
+++ lib/imgui/imconfig.h
@@ -1,8 +1,7 @@
 // This file has been customised for Principia. Reset the changes in this file when updating Imgui.
 #pragma once
 
-// TODO: uncomment this when we're good
-//#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
+#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
 
 #ifndef DEBUG
     #define IMGUI_DISABLE_DEMO_WINDOWS
@@ -14,6 +13,7 @@
 #define IMGUI_DISABLE_WIN32_DEFAULT_IME_FUNCTIONS         // [Win32] [Default with non-Visual Studio compilers] Don't implement default IME handler (won't require imm32.lib/.a)
 #define IMGUI_DISABLE_WIN32_FUNCTIONS                     // [Win32] Won't use and link with any Win32 function (clipboard, IME).
 #define IMGUI_DISABLE_DEFAULT_SHELL_FUNCTIONS             // Don't implement default platform_io.Platform_OpenInShellFn() handler (Win32: ShellExecute(), require shell32.lib/.a, Mac/Linux: use system("")).
+#define IMGUI_DISABLE_TIME_FUNCTIONS                      // Don't setup default platform_io.Platform_SessionDate value using time(), localtime_r().
 
 // We use freetype with a custom font
 #define IMGUI_DISABLE_DEFAULT_FONT
